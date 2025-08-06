ff0be5f23c6fa50c1c3e562d1d648cc4d5f1fa45
Abdoulaye <Abdulaibayo@outlook.fr>
2025-08-06 23:00:46 +0200

    2. AJAX avec fetch - Avec parametres  DONE ✅

--- Scripts_Clients/Ajax/ajax_avec_query_et_sans_jquery/vanillaJS/vue.php
+++ Scripts_Clients/Ajax/ajax_avec_query_et_sans_jquery/vanillaJS/vue.php
@@ -1,3 +1,8 @@
-<?php 
+<h1>COUCOU M8, i hope you dont forget you grind and farm at max tawakul and ihsan, and everything will be easy, and you will also feel like everything is too slow, dont forget to memorize al waqiah and think about it, also dont forget to see in absolutly everything the 99 NAMES </h1>
 
-    echo "<h1>COUCOU M8, i hope you dont forget you grind and farm at max tawakul and ihsan, and everything will be easy, and you will also feel like everything is too slow, dont forget to memorize al waqiah and think about it, also dont forget to see in absolutly everything the 99 NAMES </h1>"; die();
+<div>
+    Votre id: <?php echo $_POST['id'] ?>
+</div>
+<div>
+    Votre message: <?php echo $_POST['message'] ?>
+</div>
