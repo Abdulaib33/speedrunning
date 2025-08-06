1d77d8c2dc9e83dfa1466e0e6ab01032b744814e
Abdoulaye <Abdulaibayo@outlook.fr>
2025-08-06 22:15:09 +0200

    second commit

---
+++ Scripts_Clients/Ajax/ajax_avec_query_et_sans_jquery/vanillaJS/vue.php
@@ -0,0 +1,3 @@
+<?php 
+
+    echo "<h1>COUCOU M8, i hope you dont forget you grind and farm at max tawakul and ihsan, and everything will be easy, and you will also feel like everything is too slow, dont forget to memorize al waqiah and think about it, also dont forget to see in absolutly everything the 99 NAMES </h1>"; die();
